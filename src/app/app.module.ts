490eac3a73c326e8d112c937f68687937c514d80
greg-hammond <45513541+greg-hammond@users.noreply.github.com>
2020-02-21 19:06:06 -0600

    post-upgrade: update material components
    ng update @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { MatCardModule } from '@angular/material/card';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatSliderModule } from '@angular/material/slider';
